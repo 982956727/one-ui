898aed1070aec443253694e0eb3899fbf8b4bd08
982956727 <982956727@qq.com>
2023-11-14 23:24:30 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:982956727/one-ui-website-1.git &&
+git remote add origin git@github.com:982956727/one-ui.git &&
 git push -f -u origin master &&
 cd -
 echo https://982956727.github.io/one-ui-website/index.html
